41ff22a18a59d2d97ff23a62dcaba8bd1f66053a
Miguel Angel <miguel1998er@gmail.com>
2025-04-08 11:55:32 -0600

    implementacion de imagen en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -83,7 +83,7 @@
 <main class="form-signin">
   <form action="{{ route('auth.authenticate') }}" method="POST">
       @csrf
-    <img class="mb-4" src="{{ asset('img/bootstrap-logo.svg') }}" alt="" width="72" height="57">
+    <img class="mb-4" src="{{asset('img/gallo.jpeg')}}" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
